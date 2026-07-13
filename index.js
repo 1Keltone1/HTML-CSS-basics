631bb493e5aa667e9a63ffc36f38fb26f8cd2fda
1Keltone1 <kalininanatalia047@gmail.com>
2026-07-13 13:41:51 +0300

    Theme Switcher

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
-const tabs = document.querySelectorAll('[role="tab"]');
-const panels = document.querySelectorAll('[role="tabpanel"]');
+const themes = [
+  {
+    name: "light",
+    message: "Here comes the sun!"
+  },
+  {
+    name: "dark",
+    message: "Dark necessities are part of my design..."
+  }
+];
 
-tabs.forEach(tab => {
-  tab.addEventListener("click", () => {
-    tabs.forEach(t => t.setAttribute("aria-selected", "false"));
-    panels.forEach(p => p.hidden = true);
+let isExpanded = false;
+const list = document.getElementById("theme-dropdown");
+const btn = document.getElementById("theme-switcher-button");
+const note = document.getElementById("switch-note");
+const pageBody = document.querySelector("body");
 
-    tab.setAttribute("aria-selected", "true");
-    const associatedPanel = tab.getAttribute("aria-controls");
-    const panel = document.getElementById(associatedPanel);
-    panel.hidden = false;
-  });
+function showList() {
+  list.hidden = !list.hidden;
+  isExpanded = !isExpanded;
+  btn.setAttribute("aria-expanded", isExpanded);
+  if (isExpanded){
+    list.style.display = "flex";
+  } else {
+    list.style.display = "none";
+  }
+}
+
+btn.addEventListener("click", () => {
+  showList();
+})
+
+function choseTheme(theme) {
+  const msg = themes.find(item => item.name === theme).message;
+  showList();
+  note.innerHTML = `<p>${msg}</p>`;
+  if (theme === "light") {
+    pageBody.classList.add("light");
+    if (pageBody.classList.contains("dark")) {
+      pageBody.classList.remove("dark");
+    }
+  } else {
+    pageBody.classList.add("dark");
+    if (pageBody.classList.contains("light")) {
+      pageBody.classList.remove("light");
+    }
+  }
+}
+
+const options = document.querySelectorAll('[role="menuitem"]');
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    choseTheme(option.textContent);
+  })
 });
