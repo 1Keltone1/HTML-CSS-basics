3aa74068d9e7c13686d8b51be701c10ac94567da
1Keltone1 <kalininanatalia047@gmail.com>
2026-07-18 11:04:05 +0300

    RegEx SandBox

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
-const btn = document.getElementById("check-btn");
-const input = document.getElementById("text-input");
-const regex = /[^a-zA-Z0-9]/g;
-const result = document.getElementById("result");
+const regexPattern = document.getElementById("pattern");
+const stringToTest = document.getElementById("test-string");
+const testButton = document.getElementById("test-btn");
+const testResult = document.getElementById("result");
+const caseInsensitiveFlag = document.getElementById("i");
+const globalFlag = document.getElementById("g");
 
-function showResult() {
-  if (input.value === "") {
-    alert("Please input a value");
-  } else {
-    const str = input.value.toLowerCase().replace(regex, "");
-    const reversed = str.split("").reverse().join("");
-    if (str === reversed) {
-      result.textContent = `${input.value} is a palindrome`;
-      result.style.display = "flex";
-    } else {
-      result.textContent = `${input.value} is not a palindrome`;
-      result.style.display = "flex";
+function getFlags() {
+  let result = '';
+  if (caseInsensitiveFlag.checked) {
+    result += 'i';
+  }
+  if (globalFlag.checked) {
+    result += 'g';
+  }
+  return result;
+}
+
+
+function regexMatch() {
+  const regex = new RegExp(regexPattern.value, getFlags());
+  let testString = stringToTest.textContent;
+  let funcResult = ''
+  
+  if (regex.global) {
+    const resultArr = Array.from(testString.matchAll(regex));
+    if (resultArr.length > 0) {
+      testString = testString.replaceAll(regex, `<span class="highlight">$&</span>`);
+      const func = [];
+      for (const el of resultArr) {
+        if (typeof el[0] === 'string') {
+          func.push(el[0]);
+        }
+      }
+      funcResult = func.join(", ");
+      stringToTest.innerHTML = testString;
+      testResult.textContent = funcResult;
     }
+  } else {
+    const resultArr = testString.match(regex);
+    if (resultArr !== null) {
+      testString = testString.replace(regex, `<span class="highlight">$&</span>`);
+      funcResult = resultArr[0];
+    }
+    stringToTest.innerHTML = testString;
+    testResult.textContent = funcResult;
+  }
+  if (testResult.textContent === '') {
+    testResult.textContent = "no match";
   }
 }
 
-btn.addEventListener("click", showResult);
+testButton.addEventListener("click", regexMatch);
