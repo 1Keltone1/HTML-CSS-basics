84f29879ac08c50f2f8a6f9c86ffc35a1f587073
1Keltone1 <kalininanatalia047@gmail.com>
2026-07-20 17:24:36 +0300

    Music Player

--- index.js
+++ index.js
@@ -1,208 +1,158 @@
-const btn = document.getElementById("submit-btn");
-const orderRegex = /^2024\d{6}$/;
-const codeRegex = /^[a-zA-Z]{2}\d{2}-[a-zA-Z]\d{3}-[a-zA-Z]{2}\d$/;
-const form = document.getElementById("form");
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const playingSong = document.getElementById("player-song-title");
+const songArtist = document.getElementById("player-song-artist");
+const allSongs = [
+  {
+    id: 0,
+    title: "Hello World",
+    artist: "Rafael",
+    duration: "0:23",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/hello-world.mp3",
+  },
+  {
+    id: 1,
+    title: "In the Zone",
+    artist: "Rafael",
+    duration: "0:11",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/in-the-zone.mp3",
+  },
+  {
+    id: 2,
+    title: "Camper Cat",
+    artist: "Rafael",
+    duration: "0:21",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/camper-cat.mp3",
+  },
+  {
+    id: 3,
+    title: "Electronic",
+    artist: "Rafael",
+    duration: "0:15",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/electronic.mp3",
+  },
+  {
+    id: 4,
+    title: "Sailing Away",
+    artist: "Rafael",
+    duration: "0:22",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/sailing-away.mp3",
+  },
+];
 
-function validateForm() {
-    let name = true;
-    let email = true;
-    let order = true;
-    let code = true;
-    let quantity = true;
-    let check = false;
-    let description = true;
-    let radio = false;
-    let solution = true;
-    let other = false;
-    let otherSol = false;
+const audio = new Audio();
 
-    if (!document.getElementById("full-name").value) {
-        name = false;
-    }
-    if (!document.getElementById("email").checkValidity()) {
-        email = false;
-    }
-    if (!orderRegex.test(document.getElementById("order-no").value)) {
-        order = false;
-    }
-    if (!codeRegex.test(document.getElementById("product-code").value)) {
-        code = false;
-    }
+const userData = {
+  songs: allSongs,
+  currentSong: null,
+  songCurrentTime: 0,
+};
 
-    const quantityEl = document.getElementById("quantity").value;
-    if (quantityEl === "" || !Number.isInteger(Number(quantityEl)) || quantityEl < 1) {
-        quantity = false;
-    }
-
-    const group = document.querySelectorAll('input[name="complaint"]');
-    for (const box of group) {
-        console.log(box);
-        if (box.id == "other-complaint" && box.checked) {
-            other = true;
-            check = true;
-            break;
-        }
-        if (box.checked) {
-            check = true;
-            break;
-        }
-    }
-    
-    if (other) {
-        if (document.getElementById("complaint-description").value.length < 20) {
-            description = false;
-        }
-    }
-
-    const radioGroup = document.querySelectorAll('input[name="solutions"]');
-    for (const btn of radioGroup) {
-        if (btn.checked && btn.id == "other-solution") {
-            radio = true;
-            otherSol = true;
-            break;
-        }
-        if (btn.checked) {
-            radio = true;
-            break;
-        }
-    }
-
-    if (otherSol) {
-        if (document.getElementById("solution-description").value.length < 20) {
-            solution = false;
-        }
-    }
-
-    return {
-        "full-name": name,
-        "email": email,
-        "order-no": order,
-        "product-code": code,
-        "quantity": quantity,
-        "complaints-group": check, 
-        "complaint-description": description, 
-        "solutions-group": radio,
-        "solution-description": solution
-    }
+const playSong = (id, start=true) => {
+  const song = userData.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
+  if (userData.currentSong === null || start) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData.songCurrentTime;
+  }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
+  setPlayerDisplay();
+  highlightCurrentSong();
+  setPlayButtonAccessibleText();
+  audio.play();
 }
 
-function isValid(obj) {
-    for (const prop in obj) {
-        if(!obj[prop]) {
-            return false;
-        }
-    }
-    return true;
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime;
+  playButton.classList.remove("playing");
+  audio.pause();
 }
 
-const nameInput = document.getElementById("full-name");
-const emailInput = document.getElementById("email");
-const numInput = document.getElementById("order-no");
-const codeInput = document.getElementById("product-code");
-const quantityInput = document.getElementById("quantity");
-const complaintsField = document.getElementById("complaints-group");
-const complaintCheckboxes = document.querySelectorAll('input[name="complaint"]');
-const descriptionInput = document.getElementById("complaint-description");
-const solutionsInput = document.getElementById("solutions-group");
-const solutionRadios = document.querySelectorAll('input[name="solutions"]');
+const getCurrentSongIndex = () => userData.songs.indexOf(userData.currentSong);
 
+const getNextSong = () => userData.songs[getCurrentSongIndex() + 1];
 
-nameInput.addEventListener("change", () => {
-    let validObj = validateForm();
-    if (validObj["full-name"]) {
-        nameInput.style.borderColor = 'green';
-    } else {
-        nameInput.style.borderColor = "red";
-    }
+const getPreviousSong = () => userData.songs[getCurrentSongIndex() - 1];
+
+const playPreviousSong = () => {
+  if (userData.currentSong === null) return;
+  const previousSong = getPreviousSong();
+  if (previousSong) {
+    playSong(previousSong.id);
+  } else {
+    playSong(userData.songs[0].id);
+  }
+};
+
+const playNextSong = () => {
+  if (userData.currentSong === null) {
+    playSong(userData.songs[0].id);
+    return;
+  }
+  const nextSong = getNextSong();
+  if (nextSong) {
+    playSong(nextSong.id);
+  } else {
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    setPlayerDisplay();
+    highlightCurrentSong();
+    setPlayButtonAccessibleText();
+    pauseSong();
+  }
+}
+
+const setPlayerDisplay = () => {
+  const currentTitle = userData.currentSong?.title;
+  const currentArtist = userData.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
+const highlightCurrentSong = () => {
+  const previousCurrentSong = document.querySelector('.playlist-song[aria-current="true"]');
+  previousCurrentSong?.removeAttribute("aria-current");
+  const songToHighlight = document.getElementById(
+    `song-${userData.currentSong?.id}`
+  );
+  
+  songToHighlight?.setAttribute("aria-current", "true");
+};
+
+const setPlayButtonAccessibleText = () => {
+  const song = userData.currentSong;
+  playButton.setAttribute("aria-label", userData.currentSong ? `Play ${song.title}` : "Play");
+};
+
+playButton.addEventListener("click", () => {
+  if (userData.currentSong === null) {
+    playSong(userData.songs[0].id);
+  } else {
+    playSong(userData.currentSong.id, false);
+  }
 });
 
-emailInput.addEventListener("change", () => {
-    if (emailInput.checkValidity()) {
-        emailInput.style.borderColor = 'green';
-    } else {
-        emailInput.style.borderColor = "red";
-    }
-});
+const songs = document.querySelectorAll(".playlist-song");
 
-numInput.addEventListener("change", () => {
-    let validObj = validateForm();
-    if (validObj["order-no"]) {
-        numInput.style.borderColor = 'green';
-    } else {
-        numInput.style.borderColor = "red";
-    }
-});
+songs.forEach((song) => {
+  const id = song.getAttribute("id").slice(5);
+  const songBtn = song.querySelector("button");
+  songBtn.addEventListener("click", () => {
+    playSong(Number(id));
+  })
+})
 
-codeInput.addEventListener("change", () => {
-    let validObj = validateForm();
-    if (validObj["product-code"]) {
-        codeInput.style.borderColor = 'green';
-    } else {
-        codeInput.style.borderColor = "red";
-    }
-});
+pauseButton.addEventListener("click", pauseSong);
 
-quantityInput.addEventListener("change", () => {
-    let validObj = validateForm();
-    if (validObj["quantity"]) {
-        quantityInput.style.borderColor = 'green';
-    } else {
-        quantityInput.style.borderColor = "red";
-    }
-});
+nextButton.addEventListener("click", playNextSong);
 
-complaintCheckboxes.forEach((checkbox) => {
-    checkbox.addEventListener("change", () => {
-        let validObj = validateForm();
-        if (validObj["complaints-group"]) {
-            complaintsField.style.borderColor = 'green';
-        } else {
-            complaintsField.style.borderColor = "red";
-        }
-    });
-});
+previousButton.addEventListener("click", playPreviousSong);
 
-const otherComplaint = document.getElementById("other-complaint");
-const complaintDesc = document.getElementById("complaint-description");
-
-complaintDesc.addEventListener("input", function() {
-    if (otherComplaint.checked) {
-        if (complaintDesc.value.length >= 20) {
-            descriptionInput.style.borderColor = 'green';
-        } else {
-            descriptionInput.style.borderColor = 'red';
-        }
-    } else {
-        descriptionInput.style.borderColor = 'black';
-    }
-});
-
-solutionRadios.forEach((radio) => {
-    radio.addEventListener("change", () => {
-        let validObj = validateForm();
-        if (validObj["solutions-group"]) {
-            solutionsInput.style.borderColor = 'green';
-        } else {
-            solutionsInput.style.borderColor = "red";
-        }
-    });
-});
-
-const otherSolution = document.getElementById("other-solution");
-const solutionDesc = document.getElementById("solution-description");
-
-solutionDesc.addEventListener("input", function() {
-    if (otherSolution.checked) {
-        if (solutionDesc.value.length >= 20) {
-            solutionDesc.style.borderColor = 'green';
-        } else {
-            solutionDesc.style.borderColor = 'red';
-        }
-    } else {
-        solutionDesc.style.borderColor = '';
-    }
-});
-
-btn.addEventListener("submit", () => {
-    isValid(validateForm())
-});
+audio.addEventListener("ended", playNextSong);
