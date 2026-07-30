30e50588d9c790b19e177fa5abb113005ec044d5
1Keltone1 <kalininanatalia047@gmail.com>
2026-07-30 17:49:20 +0300

    Depth-First Search Algorithm

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-function permuteString(str, prefix="", results=[]) {
-  if (str.length === 0) {
-    if (!results.includes(prefix)){
-      results.push(prefix);
+function dfs(graph, root) {
+  const list = toList(graph);
+  const n = graph.length;
+  const result = [];
+  const visited = new Array(n).fill(false);
+
+  rec_dfs(graph, root, result, visited, list);
+  
+  return result;
+}
+
+function rec_dfs(graph, v=root, result, visited, list) {
+  visited[v] = true;
+  console.log(`v: ${v}`)
+  result.push(v);
+  for (let node of list[v] || []) {
+    if (!visited[node]) {
+      rec_dfs(graph, node, result, visited, list);
     }
-    return results;
-  } else {
-    for (let i= 0; i < str.length; i++) {
-      let newStr = str.slice(0, i) + str.slice(i + 1);
-      results = permuteString(newStr, prefix + str[i], results);
-    }
-    return results;
   }
 }
 
-console.log(permuteString("fcc"));
+function toList(graph) {
+  const list = {};
+
+  for (let i = 0; i < graph.length; i++) {
+    list[i] = [];
+    for (let j = 0; j < graph[i].length; j++) {
+      if (graph[i][j] === 1) {
+        if (!list[i].includes(j)) {
+          list[i].push(j);
+        }
+      }
+    }
+  }
+  return list;
+}
+
+console.log(JSON.stringify(dfs([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]], 1)));
