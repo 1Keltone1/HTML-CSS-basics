13b413a4c278733b4d4deac96eb11305daa022d3
1Keltone1 <kalininanatalia047@gmail.com>
2026-07-30 17:51:25 +0300

    DFS easier way

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
 function dfs(graph, root) {
-  const list = toList(graph);
   const n = graph.length;
-  const result = [];
   const visited = new Array(n).fill(false);
+  const result = [];
 
-  rec_dfs(graph, root, result, visited, list);
-  
-  return result;
-}
-
-function rec_dfs(graph, v=root, result, visited, list) {
-  visited[v] = true;
-  console.log(`v: ${v}`)
-  result.push(v);
-  for (let node of list[v] || []) {
-    if (!visited[node]) {
-      rec_dfs(graph, node, result, visited, list);
-    }
-  }
-}
-
-function toList(graph) {
-  const list = {};
-
-  for (let i = 0; i < graph.length; i++) {
-    list[i] = [];
-    for (let j = 0; j < graph[i].length; j++) {
-      if (graph[i][j] === 1) {
-        if (!list[i].includes(j)) {
-          list[i].push(j);
-        }
+  function rec_dfs(v) {
+    visited[v] = true;
+    result.push(v);
+    
+    for (let i = 0; i < n; i++) {
+      if (graph[v][i] === 1 && !visited[i]) {
+        rec_dfs(i);
       }
     }
   }
-  return list;
+
+  rec_dfs(root);
+  return result;
 }
 
-console.log(JSON.stringify(dfs([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]], 1)));
+console.log(dfs([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]], 1))
